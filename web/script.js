70cae3be2058deaae415edda5ef1d716276a8641
POGsss <frankiepinat14@gmail.com>
2025-06-02 04:15:28 +0800

    Cleaning The Database

--- web/script.js
+++ web/script.js
@@ -11,6 +11,8 @@ async function updateAnalytics() {
     console.log(data.length);
 
     document.getElementById('totalUsers').textContent = data.length.toString().padStart(3, '0');
+    document.getElementById('maleUsers').textContent = data.filter(user => user.gender === 'Male').length.toString().padStart(3, '0');
+    document.getElementById('femaleUsers').textContent = data.filter(user => user.gender === 'Female').length.toString().padStart(3, '0');
   } catch (error) {
     console.error('Error fetching analytics:', error);
   }
@@ -36,8 +38,10 @@ function renderUsers(users) {
     const card = document.createElement('div');
     card.className = 'userCardContainer';
 
+    const imageUrl = user.profile_picture ? `${BASE_URL}${user.profile_picture}` : 'assets/image_placeholder.svg';
+
     card.innerHTML = `
-      <img src="${BASE_URL}${user.profile_picture}" alt="profile">
+      <img src="${imageUrl}" alt="profile">
       <div class="userCard">
         <h3>${user.name}</h3>
         <p>${user.age}, ${user.gender}</p>
@@ -53,12 +57,29 @@ function renderUsers(users) {
 document.getElementById('searchBar').addEventListener('input', async e => {
   const query = e.target.value.toLowerCase();
   try {
-    const res = await fetch(API_URL);
+    const res = await fetch(USER_URL);
     const users = await res.json();
     const filtered = users.filter(u => u.name.toLowerCase().includes(query));
     renderUsers(filtered);
   } catch (error) {
     console.error('Error filtering users:', error);
+  }
+});
+
+// Search Icon Toggle
+document.getElementById('search').addEventListener('click', () => {
+  const searchBar = document.getElementById('searchBar');
+  searchBar.style.display = searchBar.style.display === 'block' ? 'none' : 'block';
+  searchBar.focus();
+});
+
+// Search Icon Visibility Based On Screen Width
+window.addEventListener('resize', () => {
+  const searchBar = document.getElementById('searchBar');
+  if (window.screen.width > 512) {
+    searchBar.style.display = 'block';
+  } else {
+    searchBar.style.display = 'none';
   }
 });
 
